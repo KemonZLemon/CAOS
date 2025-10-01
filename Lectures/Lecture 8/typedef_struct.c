4d23d4cebf3867a6a58bf2e4866f79bdd9cd31dd
Kevin Z <rpizhuk5@gmail.com>
2025-10-01 14:13:11 -0400

    Homeworks, Lecture Notes, and Labs

--- Lectures/Lecture 8/typedef_struct.c
+++ Lectures/Lecture 8/typedef_struct.c
@@ -1,40 +1,62 @@
+/* typedef-struct.c */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
-typedef struct {
-  unsigned int rin;
-  char rcsid[20];
+/* typedef ... <type-name> */
+
+typedef struct
+{
+  unsigned int rin;   /* e.g., 660000001 */
+  char rcsid[20];     /* e.g., "goldsd3" */
+  /* ... */
 }
 student_t;
 
-int main() {
-  printf("sizeof student_t is %lu\n", sizeof(student_t));
+int main()
+{
+  printf( "sizeof student_t is %lu\n", sizeof( student_t ) );
+  /* see: sizeof.c example... */
 
-  /* static allocations*/
+  /* static allocation */
   student_t me;
-  me.rin = 600000001;
-  strcpy(me.rcsid, "goldsd3");
+  me.rin = 660000001;
+  strcpy( me.rcsid, "goldsd3" );
 
-  /* dynamic allocations */
+  /* dynamic allocation */
   student_t * st;
-  st = malloc(sizeof(student_t));   //or calloc
-  (*st).rin = 661234567;
-  strcpy((*st).rcsid, "smithj123");
+  st = malloc( sizeof( student_t ) );   /* or calloc() */
+  (*st).rin = 661234567;    /* st->rin = 661234567; */
+  strcpy( (*st).rcsid, "smithj123" );
 
-  printf("me: RIN %u RCSID %s\n", me.rin, me.rcsid);
-  printf("me: RIN %u RCSID %s\n", st -> rin, st -> rcsid);
+  printf( "me: RIN %u; RCSID %s\n", me.rin, me.rcsid );
+  printf( "st: RIN %u; RCSID %s\n", (*st).rin, (*st).rcsid );
+  printf( "st: RIN %u; RCSID %s\n", st->rin, st->rcsid );
 
-  free(st);
-  /* dynamically alocate an array of student_t of size 7000 */
-  student_t * students = calloc(7000, sizeof(student_t));
+  free( st );
 
+  /***********************************************************/
+
+  /* dynamically allocate an array of student_t of size 7000 */
+  student_t * students = calloc( 7000, sizeof( student_t ) );
+
+  /* this is a "shallow" copy...
+   * ...which means that the assignment below only copies the
+   *     static allocations/variables
+   */
   students[500] = me;
   *(students + 500) = me;
-  printf("students[500]: RIN %u RCSID %s\n", students[500].rin, students[500].rcsid);
-  printf("students[500]: RIN %u RCSID %s\n", (*(students + 500)).rin, (*(students + 500)).rcsid);
-  printf("students[500]: RIN %u RCSID %s\n", (students + 500) -> rin, (students + 500) -> rcsid);
 
-  free(students);
+  printf( "students[500]: RIN %u; RCSID %s\n",
+         students[500].rin, students[500].rcsid );
+  printf( "students[500]: RIN %u; RCSID %s\n",
+         (*(students + 500)).rin, (*(students + 500)).rcsid );
+  printf( "students[500]: RIN %u; RCSID %s\n",
+         (students + 500)->rin, (students + 500)->rcsid );
+
+  free( students );
+
   return EXIT_SUCCESS;
 }
